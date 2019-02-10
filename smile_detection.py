a96234a30ffef28c1bec6550e60e814f62ac9147
Aytac Aydin <aydinayt93@gmail.com>
2019-02-10 22:15:54 +0300

    Update smile_detection.py

--- smile_detection.py
+++ smile_detection.py
@@ -9,13 +9,11 @@ import dlib
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
-import timeit
-
 
 # In[ ]:
 
 
-smile_detector = load_model('/home/itouch/Downloads/smile_detector_MVGGNet.hdf5')
+smile_detector = load_model('path_to_trained_model')
 face_detector = dlib.get_frontal_face_detector()
 
 
@@ -40,12 +38,9 @@ while cap.isOpened():
                 face = cv2.resize(face, (32, 32))
                 face = face.astype("float") / 255.0 # scale to range [0,1]
                 face = img_to_array(face) # convert to the model compatible format
-                face = np.expand_dims(face, axis=0)
+                face = np.expand_dims(face, axis=0)                
                 
-                tic = timeit.default_timer()
-                (notSmiling, smiling) = smile_detector.predict(face)[0]
-                toc = timeit.default_timer()
-                print('Time to detect smile: {:.2f}'.format(toc-tic))
+                (notSmiling, smiling) = smile_detector.predict(face)[0]               
                 label = "Smile" if smiling > notSmiling else "No_Smile"
                 
                 cv2.putText(frame, label, (d.left(), d.top() - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -63,4 +58,3 @@ while cap.isOpened():
 
 cap.release()
 cv2.destroyAllWindows()
-
